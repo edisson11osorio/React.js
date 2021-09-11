98500ba905cba8dfb1c1520cea84a968b1275aec
edisson <osorioedisson08@gmail.com>
2021-09-11 11:18:02 -0500

    [ADD]changes

--- fitness/src/index.js
+++ fitness/src/index.js
@@ -1,6 +1,29 @@
 import React from "react";
 import reactDom from "react-dom";
 
-const element = <h1>edisson</h1>
+const user = {
+  nombre: 'edisson',
+  apellio: 'osorio',
+  avatar: 'https://p4.wallpaperbetter.com/wallpaper/277/725/219/kimetsu-no-yaiba-giyu-tomioka-kimetsu-no-yaiba-blue-eyes-hd-wallpaper-preview.jpg'
+}
+function getName(user){
+  return `${user.nombre} ${user.apellio}`
+}
+function getGreeiting(user){
+  if(user){
+    return <h1>hello {getName(user)}</h1>
+  }
+  return <h1>s</h1>
+}
+
+const name = 'edisson'
+const element = (
+      <div>
+        <h1>{getGreeiting(user)}</h1>
+        <img src={user.avatar}/>
+      </div>
+)
+
+
 const container = document.getElementById('root')
 reactDom.render(element, container)
