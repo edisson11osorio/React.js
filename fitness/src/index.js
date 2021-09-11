eb445facc921b3ef135e7486e316c9988ea50c9a
edisson <osorioedisson08@gmail.com>
2021-09-11 10:21:20 -0500

    first_commit

---
+++ fitness/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import reactDom from "react-dom";
+
+const element = <h1>edisson</h1>
+const container = document.getElementById('root')
+reactDom.render(element, container)
