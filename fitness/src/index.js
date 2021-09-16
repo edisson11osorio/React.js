893d03d06752bf0662f214c27cf394a140c610d7
edisson <osorioedisson08@gmail.com>
2021-09-15 22:30:11 -0500

    [adding]changes

--- fitness/src/index.js
+++ fitness/src/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import reactDom from "react-dom";
 import Exercises from "./Pages/Exercises";
 import 'bootstrap/dist/css/bootstrap.css'
+
  
 const container = document.getElementById('root')
+
+
 
 reactDom.render(<Exercises/>, container)
